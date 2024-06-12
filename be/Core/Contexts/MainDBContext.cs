3d3306df7b0d7f0a33dc646caba6d9e7660e9a58
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-06-12 10:04:51 +0700

    Update: migrate from NodeTS to ASP.NET Core

---
+++ be/Core/Contexts/MainDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Models;
+using System;
+
+namespace Core.Contexts
+{
+    public class MainDBContext : DbContext
+    {
+        public MainDBContext(DbContextOptions<MainDBContext> options) : base(options) { }
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Create Users Table
+            builder.Entity<UserModel>().ToTable("users");
+        }
+    }
+}
---
+++ be/Core/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Core.Models
+{
+    public class UserModel
+    {
+        public Guid id { get; set; }
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string email { get; set; }
+        public bool isActive { get; set; } = false;
+    }
+}
